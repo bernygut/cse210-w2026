21be116088a83e3000f23cb109c0a89e8891d0bd
bernygut <bernygutierrez@gmail.com>
2026-01-17 09:34:40 -0600

    Prep 2 submission.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Diagnostics.Tracing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        int percentage;
+        bool pass;
+
+        Console.WriteLine("What was your grade percentage?");
+        percentage = int.Parse(Console.ReadLine());
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }  
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        } 
+
+        if (percentage >= 70)
+        {
+           Console.WriteLine($"Your letter grade is {letter}. You passed!"); 
+        }
+
+        else
+        {
+            Console.WriteLine($"Your letter grade is {letter}. You did not pass!");
+        }
+        
+
     }
 }
