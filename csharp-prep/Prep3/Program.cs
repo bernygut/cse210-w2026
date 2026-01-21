e219b909a8ecbe3123113c29b8a1a5be8fc59a3d
bernygut <bernygutierrez@gmail.com>
2026-01-20 21:55:40 -0600

    Prep 3 submission

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        bool isGuessed = false;
+        
+
+
+        while (isGuessed == false)
+        {
+                    Console.WriteLine($"What is the magic number? {magicNumber}");
+                    Console.WriteLine("What is your guess?");
+                    int playerNumber = int.Parse(Console.ReadLine());
+                    
+                    if (playerNumber == magicNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        isGuessed = true;
+                    }
+
+                    else if (playerNumber < magicNumber)
+                    {
+                        Console.WriteLine("Guess higher!");
+                    }
+
+                    else 
+                    {
+                        Console.WriteLine("Guess lower!");
+                    }
+        }
     }
 }
